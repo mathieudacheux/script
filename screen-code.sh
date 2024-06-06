0ade9d48ffde337ced6ad52d8948f5971bb52617
Mathieu Dacheux <mathieu.dacheux@icloud.com>
2024-06-06 14:21:24 +0200

    Changed theme

--- screen-code.sh
+++ screen-code.sh
@@ -17,9 +17,9 @@ clipboard_content=$(pbpaste)
 
 encoded_text=$(echo "$clipboard_content" | base64)
 
-encode_text=$(echo "$encoded_text" | sed -e 's/\//%2F/g' | sed -e 's/\+/%2B/g' | sed -e 's/=/%3D/g')
+encode_text=$(echo "$encoded_text" | sed -e 's/\//%2F/g' |sed -e 's/\+/%2B/g' | sed -e 's/=/%3D/g')
 
-url="https://ray.so/#code=${encode_text}&theme=breeze&language=typescript&padding=32&darkMode=true&background=true"
+url="https://ray.so/#code=${encode_text}&theme=tailwind&language=typescript&padding=32&darkMode=true&background=true"
 
 echo "$url" | pbcopy
 
